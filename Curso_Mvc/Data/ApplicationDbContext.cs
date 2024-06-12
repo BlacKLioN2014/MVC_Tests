265c3f4713d579c8e71cbc358a5ddcc848f18105
A Jiménez <abrahamijg@gmail.com>
2024-06-11 21:49:42 -0600

    inicio

---
+++ Curso_Mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Curso_Mvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Curso_Mvc.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options): base(options)
+        {
+            
+        }
+
+        //Agregar los modelos (cada modelo corresponde a una tabla en la DB
+        public DbSet<Contacto> contacto {  get; set; }
+        
+    }
+}
